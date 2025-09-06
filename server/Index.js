187b01a7ae91bb0b56ee40aa64d6cba351ee3689
vijayguhan10 <vijayguhan10@gmail.com>
2025-09-06 10:47:21 +0530

    updated the indexd file with the routing to the api of message routers

--- server/Index.js
+++ server/Index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
 const projectRoutes = require('./routes/projectRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
+app.use('/api/messages', messageRoutes);
 
 app.listen(8000, () => console.log('Server running on port 8000'));
 
