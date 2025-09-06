455565e2c7cbf704f2adb3502441c5650976dd96
vijayguhan10 <vijayguhan10@gmail.com>
2025-09-06 11:00:07 +0530

    Handeling the index routing to the notification controller

--- server/Index.js
+++ server/Index.js
@@ -3,11 +3,13 @@ const app = express();
 const authRoutes = require('./routes/authRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const messageRoutes = require('./routes/messageRoutes');
+const notificationRoutes = require('./routes/notificationRoutes');
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.listen(8000, () => console.log('Server running on port 8000'));
 
